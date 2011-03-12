1fce679ea01840b073990454e9656142cb5001fd
Bob Hanson <bob@cube2media.com>
2011-03-12 11:00:52 -0700

    refactored index page to not use tables and added flowplayer

---
+++ public/scripts/mmcrap.js
@@ -0,0 +1,80 @@
+// Macromedia crap
+function mmLoadMenus() {
+  if (window.mm_menu_1005192129_0) return;
+                        window.mm_menu_1005192129_0 = new Menu("root",228,18,"Georgia, Times New Roman, Times, serif",12,"#000099","#CCCCCC","#CCCCCC","#000084","left","middle",3,0,350,-5,7,true,true,true,0,true,true);
+  mm_menu_1005192129_0.addMenuItem("Online&nbsp;Service&nbsp;Request&nbsp;Form","window.open('contact_content.php', '_blank');");
+  mm_menu_1005192129_0.addMenuItem("Residential","location='residential.html'");
+  mm_menu_1005192129_0.addMenuItem("Commercial","location='commercial.html'");
+  mm_menu_1005192129_0.addMenuItem("Refrigeration","location='industrial.html'");
+  mm_menu_1005192129_0.addMenuItem("Duct&nbsp;Cleaning","location='duct.html'");
+  mm_menu_1005192129_0.addMenuItem("General&nbsp;Maintenance","location='general.html'");
+  mm_menu_1005192129_0.addMenuItem("New&nbsp;Construction","location='newconstruction.html'");
+  mm_menu_1005192129_0.addMenuItem("Preventative&nbsp;Maintenance","location='pm.html'");
+  mm_menu_1005192129_0.addMenuItem("AC&nbsp;&&nbsp;Power&nbsp;Rental&nbsp;Equipment","location='rental.php'");
+   mm_menu_1005192129_0.hideOnMouseOut=true;
+   mm_menu_1005192129_0.bgColor='#555555';
+   mm_menu_1005192129_0.menuBorder=1;
+   mm_menu_1005192129_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_1005192129_0.menuBorderBgColor='#777777';
+window.mm_menu_1005192459_0 = new Menu("root",180,18,"Georgia, Times New Roman, Times, serif",12,"#000099","#CCCCCC","#CCCCCC","#000084","left","middle",3,0,350,-5,7,true,true,true,0,true,true);
+  mm_menu_1005192459_0.addMenuItem("AC&nbsp;Unit&nbsp;Information","location='ac_unit_info.html'");
+  mm_menu_1005192459_0.addMenuItem("Energy&nbsp;Information","location='energy.html'");
+  mm_menu_1005192459_0.addMenuItem("Energy&nbsp;Saving&nbsp;Tips","location='energytips.html'");
+  mm_menu_1005192459_0.addMenuItem("Our&nbsp;Products","location='our_products.html'");
+  mm_menu_1005192459_0.addMenuItem("Used&nbsp;Equipment&nbsp;For&nbsp;Sale","location='usedsale.html'");
+   mm_menu_1005192459_0.hideOnMouseOut=true;
+   mm_menu_1005192459_0.bgColor='#555555';
+   mm_menu_1005192459_0.menuBorder=1;
+   mm_menu_1005192459_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_1005192459_0.menuBorderBgColor='#777777';
+                        window.mm_menu_1005193122_0 = new Menu("root",160,18,"Georgia, Times New Roman, Times, serif",12,"#000099","#CCCCCC","#CCCCCC","#000084","left","middle",3,0,350,-5,7,true,true,true,0,true,true);
+  mm_menu_1005193122_0.addMenuItem("Bally's","location='ballys.html'");
+  mm_menu_1005193122_0.addMenuItem("Harras&nbsp;Fitness","location='harras.html'");
+  mm_menu_1005193122_0.addMenuItem("PT'S&nbsp;Pub","location='ptpub.html'");
+  mm_menu_1005193122_0.addMenuItem("Quest&nbsp;Project","location='quest.html'");
+  mm_menu_1005193122_0.addMenuItem("Rancho&nbsp;Market","location='ranchomarket.html'");
+  mm_menu_1005193122_0.addMenuItem("Silverstate&nbsp;Web&nbsp;Ink","location='ssweb.html'");
+  mm_menu_1005193122_0.addMenuItem("Washington&nbsp;Mutual","location='washington.html'");
+  mm_menu_1005193122_0.addMenuItem("Whole&nbsp;Foods&nbsp;Project","location='wholefoods.html'");
+  mm_menu_1005193122_0.addMenuItem("Warehouse&nbsp;Project","location='warehouse.html'");
+  mm_menu_1005193122_0.addMenuItem("Testimonials","location='test.html'");
+   mm_menu_1005193122_0.hideOnMouseOut=true;
+   mm_menu_1005193122_0.bgColor='#555555';
+   mm_menu_1005193122_0.menuBorder=1;
+   mm_menu_1005193122_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_1005193122_0.menuBorderBgColor='#777777';
+window.mm_menu_1005200104_0 = new Menu("root",185,18,"Georgia, Times New Roman, Times, serif",12,"#000099","#CCCCCC","#CCCCCC","#000084","left","middle",3,0,350,-5,7,true,true,true,0,true,true);
+  mm_menu_1005200104_0.addMenuItem("Employment&nbsp;Opportunities","location='employment.html'");
+  mm_menu_1005200104_0.addMenuItem("Contact&nbsp;information","location='contactus.html'");
+  mm_menu_1005200104_0.addMenuItem("Location&nbsp;Map","window.open('http://www.mapquest.com/maps/map.adp?formtype=address&addtohistory=&address=971%20Empire%20Mesa%20Way&city=Henderson&state=NV&zipcode=89011%2d1800&country=US&geodiff=1', '_blank');");
+   mm_menu_1005200104_0.hideOnMouseOut=true;
+   mm_menu_1005200104_0.bgColor='#555555';
+   mm_menu_1005200104_0.menuBorder=1;
+   mm_menu_1005200104_0.menuLiteBgColor='#FFFFFF';
+   mm_menu_1005200104_0.menuBorderBgColor='#777777';
+
+mm_menu_1005200104_0.writeMenus();
+} // mmLoadMenus()
+
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
